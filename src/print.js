bb2def9dada15e8281efdca3d2c84f1347eab522
brekk <brekk@brekkbockrath.com>
2018-05-01 09:53:10 -0700

    getting cleaner, legend can be extricated

--- src/print.js
+++ src/print.js
@@ -1,6 +1,5 @@
 import chalk from 'chalk'
 import { pipe, keys, map, join, curry, padEnd, padStart } from 'f-utility'
-import { SUBJECT_LENGTH } from './constants'
 import { filetypes } from './per-commit'
 import { summarize } from './utils'
 export const drawTokens = curry((lookup, analysis, name) => {
@@ -12,17 +11,27 @@ export const autodraw = curry((lookup, analysis) =>
   pipe(keys, map(drawTokens(lookup, analysis)), join(``))(lookup)
 )
 export const colorize = curry(
-  (lookup, { date, type, hash, changes, subject, author, analysis }) => {
+  (
+    config,
+    lookup,
+    { date, type, hash, changes, subject, author, analysis }
+  ) => {
     if (type === `banner`) {
       // 28 aligns it to the end of the commit hash
       return chalk.bgWhite(
-        chalk.black(padEnd(120, ` `, padStart(28, ` `, date)))
+        chalk.black(
+          padEnd(
+            config.bannerLength,
+            ` `,
+            padStart(config.bannerIndent, ` `, date)
+          )
+        )
       )
     } else if (type === `commit`) {
       // const { style, frontend, backend, assets, devops, tests } = analysis
       const __hash = chalk.yellow(hash)
-      const __summary = summarize(SUBJECT_LENGTH, subject)
-      const __author = chalk.red(padEnd(20, ` `, author))
+      const __summary = summarize(config.subjectLength, subject)
+      const __author = chalk.red(padEnd(config.authorLength, ` `, author))
       const tokens = autodraw(lookup, analysis)
       return `${tokens} = ${__hash} - ${__summary} $ ${__author} | ${filetypes(
         changes
