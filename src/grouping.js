c1829376a456b5005aa03be5b84bf8a1c8262cf9
brekk <brekk@brekkbockrath.com>
2018-04-30 22:25:18 -0700

    commit it while it works, dammit

--- src/grouping.js
+++ src/grouping.js
@@ -13,41 +13,6 @@ export const groupBy = curry((key, arr) =>
   )
 )
 
-export const collapseSuccessiveSameAuthor = (list) => {
-  if (list.length <= 1) {
-    return list
-  }
-  return reduce((agg, curr) => {
-    const copy = [].concat(agg)
-    if (agg.length > 1) {
-      console.log(`>>>`, curr, `<<<<`, prev)
-      const prev = copy[agg.length - 1]
-      if (prev.author === curr.author) {
-        const { analysis: prior } = prev
-        const { analysis: now } = curr
-        const amended = merge(prev, {
-          subject: `[+] ${prev.subject} && ${curr.subject}`,
-          changes: merge(prev.changes, curr.changes),
-          analysis: {
-            style: prior.style || now.style,
-            frontend: prior.frontend || now.frontend,
-            backend: prior.backend || now.backend,
-            assets: prior.assets || now.assets,
-            devops: prior.devops || now.devops,
-            tests: prior.tests || now.tests
-          }
-        })
-        // eslint-disable-next-line fp/no-mutation
-        copy[agg.length - 1] = amended
-      } else {
-        // eslint-disable-next-line fp/no-mutating-methods
-        copy.push(curr)
-      }
-    }
-    return copy
-  }, [])
-}
-
 const getNumber = (x) => Number(x.substr(0, x.indexOf(` `)))
 
 export const createBannersFromGroups = (grouped) =>
