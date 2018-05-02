e9569fb50663bb46166d5176fd3d1acb6192a87a
brekk <brekk@brekkbockrath.com>
2018-05-02 08:41:27 -0700

    pretty close to ready

--- package-scripts.js
+++ package-scripts.js
@@ -1,12 +1,40 @@
 const germs = require(`germs`)
 const pkg = require(`./package.json`)
 const utils = require(`nps-utils`)
+const { merge } = require(`f-utility`)
+const allNPS = utils.concurrent.nps
+//
+const built = [`del coverage`, `del lib`, `del docs`]
 
 const GERMS = germs.build(pkg.name, {
   readme: `echo "documentation readme -s API src/*.js"`,
-  prepublishOnly: `nps care`
+  prepublishOnly: `nps care`,
+  clean: utils.concurrent(built),
+  scrub: utils.concurrent(
+    built.concat([`del dependenc*`, `del yarn.lock`, `del node_modules`])
+  )
 })
-
+/* eslint-disable fp/no-mutation */
+GERMS.scripts.docs = {
+  script: `documentation build -c documentation.yml src/index.js -f md -o docs/API.md -a public`,
+  description: `build docs`
+}
+GERMS.scripts.bundle = merge(GERMS.scripts.bundle, {
+  debug: {
+    script: `rollup -c rollup/debug.commonjs.js`,
+    description: `generate debug version`
+  }
+})
+GERMS.scripts.bundle.script = allNPS(
+  `bundle.commonjs`,
+  `bundle.es6`,
+  `bundle.debug`
+)
+GERMS.scripts.lint.project = `clinton`
 GERMS.scripts.lint.jsdoc = `echo "documentation lint"`
+GERMS.scripts.lint = merge(GERMS.scripts.lint, {
+  script: allNPS(`lint.src`, `lint.jsdoc`, `lint.project`)
+})
+/* eslint-enable fp/no-mutation */
 
 module.exports = GERMS
--- src/grouping.js
+++ src/grouping.js
@@ -1,28 +1,16 @@
-import { merge, curry, reduce, keys, pipe } from 'f-utility'
-import { neue } from './utils'
-
-export const groupBy = curry((key, arr) =>
-  reduce(
-    (agg, raw) => {
-      const copy = neue(agg)
-      const { [key]: grouping } = raw
-      const current = copy[grouping] || []
-      const newGroup = current.concat(raw) // eslint-disable-line fp/no-mutation
-      return merge(copy, { [grouping]: newGroup })
-    },
-    {},
-    arr
-  )
-)
-
-// const getNumber = (x) => Number(x.substr(0, x.indexOf(` `)))
+import { reduce, keys, pipe } from 'f-utility'
+import { sortByDateObject, sortByAuthorDate } from './utils'
 
 export const createBannersFromGroups = (grouped) =>
   pipe(
     keys,
-    // eslint-disable-next-line fp/no-mutating-methods
-    (k) => k.sort((a, b) => Date(a) - Date(b)),
-    reduce((list, key) => {
-      return list.concat({ date: key, type: `banner` }, grouped[key])
-    }, [])
+    sortByDateObject,
+    reduce(
+      (list, key) =>
+        list.concat(
+          { date: key, type: `banner` },
+          sortByAuthorDate(grouped[key])
+        ),
+      []
+    )
   )(grouped)
