f1dfd1078337b768c7affb941cd31409df29331a
brekk <brekk@brekkbockrath.com>
2018-05-07 06:44:22 -0700

    p

--- src/constants.js
+++ src/constants.js
@@ -26,8 +26,28 @@ export const LEGEND = {
     matches: [`**/package.json`, `**/yarn.lock`]
   }
 }
+
 export const TOTAL_COMMITS = 100
 export const SUBJECT_LENGTH = 50
 export const BANNER_LENGTH = 120
 export const BANNER_INDENT = 25
 export const AUTHOR_LENGTH = 7
+
+export const DEFAULT_CONFIG = {
+  collapseMergeCommits: true,
+  collapseAuthors: false,
+  authorLength: AUTHOR_LENGTH,
+  subjectLength: SUBJECT_LENGTH,
+  bannerLength: BANNER_LENGTH,
+  bannerIndent: BANNER_INDENT,
+  repo: process.cwd(),
+  number: TOTAL_COMMITS,
+  fields: [
+    `abbrevHash`,
+    `subject`,
+    `authorName`,
+    `authorDate`,
+    `authorDateRel`
+  ],
+  execOptions: { maxBuffer: 1000 * 1024 }
+}
