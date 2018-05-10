5e131fb11ef9e366f235cad3de1adb0fc6df9e53
brekk <brekk@brekkbockrath.com>
2018-05-10 14:21:16 -0700

    passing tests again

--- src/gitparty.spec.js
+++ src/gitparty.spec.js
@@ -1,20 +1,20 @@
-import path from 'path'
-import test from 'jest-t-assert'
-import stripColor from 'strip-color'
-import { I } from 'f-utility'
-import Future from 'fluture'
+import path from "path"
+import test from "jest-t-assert"
+import stripColor from "strip-color"
+import { I } from "f-utility"
+import Future from "fluture"
 import {
   // write,
   partyData,
   partyPrint,
   processGitCommits,
   remapConfigData
-} from './gitparty'
-import RAW_LEGEND from './gitpartyrc.fixture.json'
-import harness from './data.fixture.json'
-import { generateReport } from './gitparty'
-import { DEFAULT_CONFIG } from './constants'
-import { j2, neue } from './utils'
+} from "./gitparty"
+import RAW_LEGEND from "./gitpartyrc.fixture.json"
+import harness from "./data.fixture.json"
+import { generateReport } from "./gitparty"
+import { DEFAULT_CONFIG } from "./constants"
+import { j2, neue } from "./utils"
 const EXAMPLE_LEGEND = remapConfigData(RAW_LEGEND)
 
 test(`partyData`, (t) => {
@@ -199,10 +199,8 @@ test(`partyPrint`, (t) => {
 test(`generateReport`, (t) => {
   const commits = harness.filter(({ type }) => type === `commit`)
   const CONF = neue(DEFAULT_CONFIG)
-  CONF.authorLength = 5; // eslint-disable-line
-  const output = stripColor(
-    generateReport(CONF, EXAMPLE_LEGEND, [commits[commits.length - 1]])
-  )
+  CONF.authorLength = 5 // eslint-disable-line
+  const output = stripColor(generateReport(CONF, EXAMPLE_LEGEND, [commits[commits.length - 1]]))
   /* eslint-disable max-len */
   t.is(
     output,
@@ -219,10 +217,8 @@ test(`generateReport`, (t) => {
 test(`generateReport with config.json`, (t) => {
   const commits = harness.filter(({ type }) => type === `commit`)
   const CONF = neue(DEFAULT_CONFIG)
-  CONF.j = true; // eslint-disable-line
-  const output = stripColor(
-    generateReport(CONF, EXAMPLE_LEGEND, [commits[commits.length - 1]])
-  )
+  CONF.j = true // eslint-disable-line
+  const output = stripColor(generateReport(CONF, EXAMPLE_LEGEND, [commits[commits.length - 1]]))
   /* eslint-disable max-len */
   t.deepEqual(JSON.parse(output), [
     {
@@ -335,43 +331,49 @@ test.cb(`processGitCommits`, (t) => {
   const outputF = F.chain(processGitCommits(CONF))
   outputF.fork(I, (result) => {
     /* eslint-disable max-len */
-    t.deepEqual(stripColor(result).split(`\n`), [
-      `LEGEND:  J  = js  L  = lint  T  = tests  G  = gitpartyrc  C  = config  D  = dependencies`,
-      ``,
-      `                  09-05-2018                                                                                            `,
-      ` J  L  T           = 4661430 - added a readme                                        $ brekk | eslintrc js md png`,
-      `                  08-05-2018                                                                                            `,
-      ` J                 = 9210f41 - refactor                                              $ brekk | js`,
-      ` J                 = f64e517 - everything futurized but currently requires double... $ brekk | js`,
-      ` J                 = aaa4e6c - more fluturization                                    $ brekk | js`,
-      ` J                 = 40e3dbc - added fluture and clarifying intent                   $ brekk | js`,
-      ` J           C  D  = c7f3eb7 - moving things around for the bin script               $ brekk | js json`,
-      ` J     T     C  D  = 8886271 - this will likely break non-relative tests             $ brekk | js json lock`,
-      `                  07-05-2018                                                                                            `,
-      ` J  L  T  G        = fb50fbb - tests                                                 $ brekk | eslintrc gitpartyrc js json`,
-      ` J        G        = fa928f4 - gitpartyrc                                            $ brekk | gitpartyrc js yml`,
-      ` J           C  D  = f9e5c4f - added yaml config                                     $ brekk | js json lock yml`,
-      ` J                 = 925a86e - getting cleaner                                       $ brekk | js`,
-      ` J           C  D  = c2e257b - working again                                         $ brekk | js json lock yml`,
-      `                  02-05-2018                                                                                            `,
-      ` J           C  D  = b9d98d9 - process.cwd() over __dirname                          $ brekk | js json`,
-      ` J                 = d9249be - pass lint                                             $ brekk | js`,
-      ` J                 = c5f2766 - added some date grossness for now                     $ brekk | js`,
-      ` J                 = e9569fb - pretty close to ready                                 $ brekk | js`,
-      `                  01-05-2018                                                                                            `,
-      ` J     T           = f3cc824 - utils covered                                         $ brekk | js`,
-      ` J     T     C  D  = 08c0a46 - tests!                                                $ brekk | js json lock`,
-      ` J  L              = 56b6a81 - cleanification                                        $ brekk | eslintrc js`,
-      ` J                 = 8ea8fe8 - partial change                                        $ brekk | js`,
-      ` J                 = bb2def9 - getting cleaner, legend can be extricated             $ brekk | js`,
-      ` J                 = 21f377f - legend is almost fully extracted                      $ brekk | js`,
-      ` J           C  D  = 852f7ac - add blob matching, start to clean up legend makery    $ brekk | js json lock`,
-      ` J           C  D  = 322b8d0 - cleanups and more fp                                  $ brekk | js json lock`,
-      `                  30-04-2018                                                                                            `,
-      ` J                 = c182937 - commit it while it works, dammit                      $ brekk | js`,
-      ` J           C  D  = 58b0786 - better                                                $ brekk | js json lock`,
-      ` J  L        C  D  = 1c5ffd2 - initial commit                                        $ brekk | babelrc eslintrc gitignore js json lock madgerc npmignore yml`
-    ])
+    t.is(
+      stripColor(result),
+      [
+        `LEGEND:  J  = js  L  = lint  T  = tests  G  = gitpartyrc  C  = config  D  = dependencies`,
+        ``,
+        `                  10-05-2018                                                                                            `,
+        ` J     T           = 91ff852 - passing tests again                                   $ brekk | js`,
+        ` J     T  G  C  D  = 8a4f3a9 - nearing 100% coverage                                 $ brekk | gitpartyrc js json lock`,
+        `                  09-05-2018                                                                                            `,
+        ` J  L  T           = 4661430 - added a readme                                        $ brekk | eslintrc js md png`,
+        `                  08-05-2018                                                                                            `,
+        ` J                 = 9210f41 - refactor                                              $ brekk | js`,
+        ` J                 = f64e517 - everything futurized but currently requires double... $ brekk | js`,
+        ` J                 = aaa4e6c - more fluturization                                    $ brekk | js`,
+        ` J                 = 40e3dbc - added fluture and clarifying intent                   $ brekk | js`,
+        ` J           C  D  = c7f3eb7 - moving things around for the bin script               $ brekk | js json`,
+        ` J     T     C  D  = 8886271 - this will likely break non-relative tests             $ brekk | js json lock`,
+        `                  07-05-2018                                                                                            `,
+        ` J  L  T  G        = fb50fbb - tests                                                 $ brekk | eslintrc gitpartyrc js json`,
+        ` J        G        = fa928f4 - gitpartyrc                                            $ brekk | gitpartyrc js yml`,
+        ` J           C  D  = f9e5c4f - added yaml config                                     $ brekk | js json lock yml`,
+        ` J                 = 925a86e - getting cleaner                                       $ brekk | js`,
+        ` J           C  D  = c2e257b - working again                                         $ brekk | js json lock yml`,
+        `                  02-05-2018                                                                                            `,
+        ` J           C  D  = b9d98d9 - process.cwd() over __dirname                          $ brekk | js json`,
+        ` J                 = d9249be - pass lint                                             $ brekk | js`,
+        ` J                 = c5f2766 - added some date grossness for now                     $ brekk | js`,
+        ` J                 = e9569fb - pretty close to ready                                 $ brekk | js`,
+        `                  01-05-2018                                                                                            `,
+        ` J     T           = f3cc824 - utils covered                                         $ brekk | js`,
+        ` J     T     C  D  = 08c0a46 - tests!                                                $ brekk | js json lock`,
+        ` J  L              = 56b6a81 - cleanification                                        $ brekk | eslintrc js`,
+        ` J                 = 8ea8fe8 - partial change                                        $ brekk | js`,
+        ` J                 = bb2def9 - getting cleaner, legend can be extricated             $ brekk | js`,
+        ` J                 = 21f377f - legend is almost fully extracted                      $ brekk | js`,
+        ` J           C  D  = 852f7ac - add blob matching, start to clean up legend makery    $ brekk | js json lock`,
+        ` J           C  D  = 322b8d0 - cleanups and more fp                                  $ brekk | js json lock`,
+        `                  30-04-2018                                                                                            `,
+        ` J                 = c182937 - commit it while it works, dammit                      $ brekk | js`,
+        ` J           C  D  = 58b0786 - better                                                $ brekk | js json lock`,
+        ` J  L        C  D  = 1c5ffd2 - initial commit                                        $ brekk | babelrc eslintrc gitignore js json lock madgerc npmignore yml`
+      ].join(`\n`)
+    )
     /* eslint-enable max-len */
     t.end()
   })
