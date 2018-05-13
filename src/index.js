db2c69639954ac9fcf7dff2afebe60b4052d3224
brekk <brekk@brekkbockrath.com>
2018-05-12 22:05:58 -0700

    fixed alias double-up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import path from 'path'
-import parseArgs from 'minimist'
-import { ARGV_CONFIG } from './constants'
-import { processAndPrintWithConfig } from './gitparty'
+import path from "path"
+import parseArgs from "minimist"
+import { ARGV_CONFIG } from "./constants"
+import { processAndPrintWithConfig } from "./gitparty"
 
 const config = parseArgs(process.argv.slice(2), ARGV_CONFIG)
 
-const input = path.resolve(process.cwd(), config.c || `./.gitpartyrc`)
+const input = path.resolve(process.cwd(), config.config || `./.gitpartyrc`)
 
 processAndPrintWithConfig(config, input)
