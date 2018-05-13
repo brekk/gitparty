f991eb94e618e762131d8e8211bef0ec1e9e0512
brekk <brekk@brekkbockrath.com>
2018-05-12 19:17:47 -0700

    grouping actually works

--- src/grouping.js
+++ src/grouping.js
@@ -2,8 +2,8 @@ import chalk from "chalk"
 import { ternary, filter, I, curry, merge, reduce, keys, pipe, chain } from "f-utility"
 import { uniq } from "lodash"
 import mergeOptions from "merge-options"
-import { sortByDateObject, sortByAuthorDate, neue } from "./utils"
-const uniquelyTruthy = pipe(filter(I), uniq)
+import { sortByDate, sortByDateObject, sortByAuthorDate, neue } from "./utils"
+import { getCanon } from "./alias"
 
 export const createBannersFromGroups = (grouped) =>
   pipe(
@@ -15,39 +15,37 @@ export const createBannersFromGroups = (grouped) =>
     )
   )(grouped)
 
+const quash = pipe(neue, filter(I), uniq)
 const smoosh = pipe(neue, uniq)
 const orMerge = curry((x, y) =>
   pipe(keys, reduce((out, key) => merge(out, { [key]: x[key] || y[key] }), {}))(x)
 )
 const conditionalLog = curry((condition, a, b) => ternary(condition, I, console.log)(a, b))
-export const collapseSuccessiveSameAuthor = reduce((list, next) => {
-  const clog = I // conditionalLog(false)
-  const copy = neue(list)
-  const last = copy[copy.length - 1]
-  if (next && last) {
-    clog(chalk.red(last.hash))
-    clog(chalk.inverse(`             ${last.date}                 `))
-    clog(chalk.blue(next.hash + `   ?`))
-    const authorsMatch = last.authorName === next.authorName
-    const datesMatch = last.date === next.date
-    if (authorsMatch && datesMatch) {
-      clog(`    merging`, (last.hashes || last.hash) + ` + ` + next.hash, copy.length)
-      const raw = mergeOptions(last, next)
-      const files = smoosh(last.files, next.files)
-      const subject = `${last.subject} + ${next.subject}`
-      const analysis = orMerge(last.analysis, next.analysis)
-      const augmented = mergeOptions(raw, {
-        hash: next.hash,
-        files,
-        subject,
-        analysis,
-        multiple: true,
-        hashes: uniquelyTruthy([].concat(raw.hashes, last.hash, next.hash))
-      })
-      copy[copy.length - 1] = augmented
-      return copy
+export const collapseSuccessiveSameAuthor = pipe(
+  sortByDate,
+  reduce((list, next) => {
+    const copy = neue(list)
+    const last = copy[copy.length - 1]
+    if (next && last) {
+      const authorsMatch = getCanon(last.authorName) === getCanon(next.authorName)
+      const datesMatch = last.date === next.date
+      if (authorsMatch && datesMatch) {
+        const raw = mergeOptions(last, next)
+        const files = smoosh(last.files, next.files)
+        const subject = `${last.subject} + ${next.subject}`
+        const analysis = orMerge(last.analysis, next.analysis)
+        const augmented = mergeOptions(raw, {
+          hash: next.hash,
+          files,
+          subject,
+          analysis,
+          multiple: true,
+          hashes: quash([].concat(raw.hashes, last.hash, next.hash))
+        })
+        copy[copy.length - 1] = augmented
+        return sortByDate(copy)
+      }
     }
-  }
-  clog(`no merge`, copy.length + 1)
-  return copy.concat(next)
-}, [])
+    return sortByDate(copy.concat(next))
+  }, [])
+)
