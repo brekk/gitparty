55810727a636e0018465b092c66dfafe130f45e6
brekk <brekk@brekkbockrath.com>
2018-08-22 16:25:42 -0700

    deal with error cases

--- src/gitparty.js
+++ src/gitparty.js
@@ -14,10 +14,10 @@ import {
   curry,
   chain,
   fork,
-  isObject
+  isObject,
 } from "f-utility"
 import chalk from "chalk"
-import { encase } from "fluture"
+import { encase, encaseN, Future } from "fluture"
 import { CHARACTER_LITERALS, ARGV_CONFIG } from "./constants"
 import { filterByStringPattern } from "./filters"
 import { canonize } from "./alias"
@@ -31,7 +31,7 @@ import {
   log,
   warn,
   preferredProp,
-  unaryCallbackToFuture
+  unaryCallbackToFuture,
 } from "./utils"
 import { isAMergeCommit } from "./filters"
 import { printLegend } from "./legend"
@@ -40,13 +40,24 @@ import {
   addAnalysisPerCommit,
   addTimestampPerCommit,
   addAliasesPerCommit,
-  convertStatusAndFilesPerCommit
+  convertStatusAndFilesPerCommit,
 } from "./per-commit"
 import { DEFAULT_CONFIG } from "./constants"
+import { trace } from "xtrace"
 
 const { NEWLINE } = CHARACTER_LITERALS
 
 const gotLog = encase(gitlog)
+
+// fs.access(file, fs.constants.F_OK, (err) => {
+//   console.log(`${file} ${err ? 'does not exist' : 'exists'}`);
+// });
+// const fileIsF = encaseN2(flip(fs.access)(fs.constants.F_OK))
+const fileOrError = file =>
+  new Future((rej, res) => fs.access(file, fs.constants.F_OK, e => (e ? rej(e) : res(true))))
+
+// const fileExists = file =>
+//   new Future((rej, res) => fs.access(file, fs.constants.F_OK, e => (e ? rej(false) : res(true))))
 
 /**
 @method perCommit
@@ -59,8 +70,8 @@ const perCommit = curry((lookup, x) =>
     addTimestampPerCommit,
     addAliasesPerCommit,
     macroLens(convertStatusAndFilesPerCommit, `changes`),
-    addAnalysisPerCommit(lookup)
-  )(x)
+    addAnalysisPerCommit(lookup),
+  )(x),
 )
 
 /**
@@ -82,7 +93,7 @@ export const partyData = curry((config, lookup, data) => {
     map(perCommit(lookup)),
     hasStringFilter ? filterByStringPattern(f) : I,
     collapseAuthors ? collapseSuccessiveSameAuthor(lookup) : I,
-    insertBanners
+    insertBanners,
   )(data)
 })
 
@@ -96,8 +107,8 @@ export const partyData = curry((config, lookup, data) => {
 export const partyPrint = curry((config, lookup, input) =>
   pipe(
     map(colorize(config, lookup)),
-    join(NEWLINE)
-  )(input)
+    join(NEWLINE),
+  )(input),
 )
 /**
 @method prependLegend
@@ -116,10 +127,8 @@ const prependLegend = curry((lookup, x) => printLegend(lookup) + NEWLINE + x)
 export const sideEffect = curry((effect, y) =>
   pipe(
     entries,
-    map(
-      ([k, v]) => (Array.isArray(v) ? v.map(x => effect(k, x)) : effect(k, v))
-    )
-  )(y)
+    map(([k, v]) => (Array.isArray(v) ? v.map(x => effect(k, x)) : effect(k, v))),
+  )(y),
 )
 
 /**
@@ -142,16 +151,16 @@ export const generateReport = curry((config, lookup, data) => {
       ? j2
       : pipe(
           partyPrint(config, filteredLookup),
-          prependLegend(filteredLookup)
-        )
+          prependLegend(filteredLookup),
+        ),
   )(data)
 })
 
 export const processGitCommits = curry((config, lookup) =>
   pipe(
     gotLog,
-    map(generateReport(config, lookup))
-  )(config)
+    map(generateReport(config, lookup)),
+  )(config),
 )
 
 // || v.filter)
@@ -161,16 +170,16 @@ export const remapConfigData = map(
     isObject(v) && v.matches
       ? merge(v, {
           fn: chalk[v.color],
-          matches: v.matches.map(stripDoubleBackslash)
+          matches: v.matches.map(stripDoubleBackslash),
         })
-      : v
+      : v,
 )
 /* istanbul ignore next */
 export const writeFile = binaryCallback(
   /* istanbul ignore next */
   fs.writeFile.bind(fs),
   /* istanbul ignore next */
-  e => log(e || `Wrote to file`)
+  e => log(e || `Wrote to file`),
 )
 export const reader = unaryCallbackToFuture(read.yaml)
 
@@ -178,27 +187,59 @@ export const processData = curry((config, F) =>
   chain(
     pipe(
       remapConfigData,
-      processGitCommits(merge(DEFAULT_CONFIG, config))
-    )
-  )(F)
+      processGitCommits(merge(DEFAULT_CONFIG, config)),
+    ),
+  )(F),
 )
+
+const MAKE_A_GITPARTYRC_FILE = [
+  `Unable to find a .gitpartyrc config file in this folder.`,
+  `Did you mean to create one with \`${chalk.yellow(`gitparty init`)}\`?`,
+].join(" ")
+
+const IT_ONLY_WORKS_IN_GIT_REPOS = `gitparty only works in git repositories! Did you mean to \`${chalk.yellow(
+  `git init`,
+)}\` first?`
+
+const handleKnownBadThings = e => {
+  if (e) {
+    if (e.code && e.path && e.code === `ENOENT`) {
+      if (e.path.substr(e.path.lastIndexOf(`/`)) === `/.gitpartyrc`) {
+        warn(new Error(MAKE_A_GITPARTYRC_FILE))
+        return
+      } else if (e.path === ".git/index") {
+        warn(new Error(IT_ONLY_WORKS_IN_GIT_REPOS))
+        return
+      }
+    }
+    // if (e.status)
+    warn(e)
+  }
+}
 
 /* istanbul ignore next */
 export const warnWriteOrLog = curry(
   /* istanbul ignore next */
   ({ o }, F) =>
     /* istanbul ignore next */
-    fork(warn, o ? writeFile(o) : log)(F)
+    fork(handleKnownBadThings, o ? writeFile(o) : log)(F),
 )
+
+export const isGitDirectory = dir =>
+  fs.access(file, fs.constants.F_OK, err => {
+    console.log(`${file} ${err ? "does not exist" : "exists"}`)
+  })
 
 /* istanbul ignore next */
 export const processAndPrintWithConfig = curry(
   /* istanbul ignore next */
-  (config, input) =>
+  (config, input) => {
+    // console.log(fileExists(".git/index").fork(console.log, console.log), `<<<<<<`)
     /* istanbul ignore next */
-    pipe(
-      reader,
+    return pipe(
+      i => fileOrError(".git/index").and(reader(i)),
       processData(config),
-      warnWriteOrLog(config)
+      warnWriteOrLog(config),
     )(input)
+  },
 )
